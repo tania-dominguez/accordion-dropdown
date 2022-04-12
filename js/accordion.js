4cd8946098a9bb707a570b64808ea008fd3e32ee
Tania Domínguez <80347063+tania-dominguez@users.noreply.github.com>
2022-04-12 14:27:18 +0200

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,31 @@
+if ($(".accordion__item__header").length > 0) {
+  var active = "active";
+  $(".accordion__item__header").click(function () {
+    $(this).toggleClass(active);
+    $(this).next("div").slideToggle(200);
+  });
+}
+
+if ($(".accordion__item__headerr").length > 0) {
+  var active = "active";
+  $(".accordion__item__headerr").click(function () {
+    $(this).toggleClass(active);
+    $(this).next("div").slideToggle(200);
+  });
+}
+
+if ($(".accordion__item__header_993300").length > 0) {
+  var active = "active";
+  $(".accordion__item__header_993300").click(function () {
+    $(this).toggleClass(active);
+    $(this).next("div").slideToggle(200);
+  });
+}
+
+if ($(".accordion__item__header_grey").length > 0) {
+  var active = "active";
+  $(".accordion__item__header_grey").click(function () {
+    $(this).toggleClass(active);
+    $(this).next("div").slideToggle(200);
+  });
+}
